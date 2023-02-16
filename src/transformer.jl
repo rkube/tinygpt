a4c24359cc3e0adf59241f1165ae6fb0b7ce232e
Ralph Kube <ralph_kube@gmx.net>
2023-02-16 17:54:34 -0500

    Working transformer

--- src/transformer.jl
+++ src/transformer.jl
@@ -8,12 +8,12 @@ using Zygote
 
 # block size is the length of tokens that are fed into the transformer at once
 # When fed into the transformer, it means that there are block_size examples.
-block_size = 8
+block_size = 64
 
 # Number of sequences that are trained on in parallel
-batch_size = 4
+batch_size = 8
 # Learning rate for optimization
-lr = 1e-3
+lr = 3e-4
 # Number of epochs to train on
 num_epochs = 5_000
 # Output interval
@@ -21,8 +21,9 @@ output_interval = 100
 # Average loss in this window
 avg_interval = 10
 
-n_embed = 32  # Embedding dimension
-head_size = n_embed # Set these equal for now
+n_embed = 128  # Embedding dimension
+n_heads = 4    # n_embed needs to be divisible by n_head
+
 
 
 """
@@ -144,6 +145,8 @@ end
 """
     Forward-pass for self-attention
 
+This performs causal self-attention by appliying a mask to remove communication to future tokens.
+
 Call this like
 
 > my_sha = self_attn(32, 32)
@@ -152,11 +155,9 @@ Call this like
 function (sha::self_attn)(x)
     # Get size of data we operate on
     C, T, B = size(x)   # C: Embedding size, T=Sequence length, B=Batch size
-    # Calculate key, query, value
-    k = sha.key(x) # head_size, Token, batch
+    k = sha.key(x) # head_size, Token, batch  (C, T, B)
     q = sha.query(x)
     v = sha.value(x)
-    # Perform causal self-attention
     # So far, no communication has happened. To get cross-affinity use batched multiplication from Transformers
     wts = Transformers.batchedmul(q, k, transA=true) ./ sqrt(1f0 * C)   
     # Now add the mask. Set the upper right triangular part to large negative values
@@ -182,48 +183,114 @@ ps_sha[3] == my_sha.value.weight
 """
 
 struct multihead_attn
-    heads
+    heads # A list of self-attention heads.
+    proj::Dense # Dense projection layer
+    d::Dropout
 end
 
 Flux.@functor multihead_attn
 #Flux.trainable(s::self_attn) = (s.key.weight, s.query.weight, s.value.weight)
 
 
-function multihead_attn(num_heads, head_size)
-    hh = [self_attn(head_size, head_size ÷ num_heads) for _ ∈ 1:num_heads]
-    multihead_attn(hh)
+function multihead_attn(n_embed, n_heads, head_size)
+    hh = [self_attn(n_embed, head_size) for _ ∈ 1:n_heads]
+    multihead_attn(hh, Dense(head_size * n_heads, n_embed), Dropout(0.2))
 end
 
 function (mha::multihead_attn)(x)
     # Apply each head in parallel to x. concatenate output.
-    Parallel(vcat, mha.heads...)(x)
+    x = Parallel(vcat, mha.heads...)(x)
+    x = mha.proj(x)
+    x = mha.d(x)
 end
 
 # Test if output works
-my_mha = multihead_attn(4, n_embed)
+my_mha = multihead_attn(n_embed, n_heads, n_embed ÷ n_heads)
 my_mha(out)
 
 size(my_mha(out)) == (32, 8, 1)
 
 ps_mha = Flux.params(my_mha)
-length(ps_mha) == 3 * length(my_mha.heads) # 3 parameters per self-attention head
+length(ps_mha) == 3 * length(my_mha.heads) + 2# 3 parameters per self-attention head
 ps_mha[1] == my_mha.heads[1].key.weight
 ps_mha[2] == my_mha.heads[1].query.weight
 ps_mha[3] == my_mha.heads[1].value.weight
 ps_mha[4] == my_mha.heads[2].key.weight
 ps_mha[8] == my_mha.heads[3].query.weight
 ps_mha[12] == my_mha.heads[4].value.weight
+ps_mha[13] == my_mha.proj.weight
+
+"""
+    Transformer blocks
+"""
+
+struct block
+    head_size::Int      # Attention head size
+    sa::multihead_attn  # Multi-head attention layer
+    ffwd::Chain         # Dense feed-forward model
+    ln1::LayerNorm      # Layer normalisation
+    ln2::LayerNorm
+end
+
+Flux.@functor block
+
+function block(n_embed, n_heads)
+    head_size = n_embed ÷ n_heads
+    block(head_size, 
+          multihead_attn(n_embed, n_heads, head_size), 
+          Chain(Dense(n_embed, n_heads * n_embed, relu), Dense(n_heads * n_embed, n_embed), Dropout(0.2)),
+          LayerNorm(n_embed),
+          LayerNorm(n_embed))
+end
+
+
+"""
+    Forward call
+
+Implements skip connection both to self-attention and the feed-forward
+
+"""
+function (b::block)(x)
+    x = x .+ b.sa(b.ln1(x))    # Skip connection to self-attention
+    x + b.ffwd(b.ln2(x))       # Skip connection to feed-forward
+end
+
+
+my_b = block(n_embed, n_heads)
+my_b(out)
+
+# Test that the number of parameters is correct
+# 1. Number of parameters for mulit-head attention should be equal to previously instantiated struct
+length(Flux.params(my_b.sa)) == length(Flux.params(my_mha))
+# 2. Number parameters for feed-forward models should be correct
+length(Flux.params(my_b.sa)) + length(Flux.params(my_b.ffwd)) == length(Flux.params(my_b))
+
+
+
+"""
+    Now the actual model
+"""
+
+model = Chain(my_embed(n_embed, vocab_size, block_size), 
+              block(n_embed, n_heads),
+              block(n_embed, n_heads),
+              block(n_embed, n_heads),
+              Dense(n_embed, vocab_size))
+
+
 
 
 # Remember, the dense layer eventually calculates the logits.
-model = Chain(my_embed(n_embed, vocab_size, block_size), 
-              multihead_attn(4, n_embed),     # (C, T, B)
-              Dense(n_embed, n_embed, relu),  # Adding a feed-forward layer. This applies on a per-token level (time-code 1:25:00). Shape (C, T, B)
-              Dense(n_embed, vocab_size))
+#model = Chain(my_embed(n_embed, vocab_size, block_size), 
+#              multihead_attn(4, n_embed),     # (C, T, B)
+#              Dense(n_embed, n_embed, relu),  # Adding a feed-forward layer. This applies on a per-token level (time-code 1:25:00). Shape (C, T, B)
+#              Dense(n_embed, vocab_size))
 
 # Test if we the model accepts token sequences as input
 sample_ix = rand(1:vocab_size, 20, 1)
 model(sample_ix)
+
+
 
 
 # Calculate loss. Should by -log(1/65)
@@ -272,14 +339,16 @@ for epoch ∈ 1:num_epochs
     Flux.update!(opt, ps, grad)
     # Estimate losses every output epochs
     if epoch % output_interval == 0
+        loss_dict = Dict()
         for split ∈ ["train", "test"]
             loss_i = 0.0
             for i ∈ 1:avg_interval
                 (xb, yb) = get_batch(split)
                 loss_i += loss(xb, yb)
             end
-            println("Epoch $(epoch): $(split): loss = $(loss_i / avg_interval)")
+            loss_dict[split] = loss_i / avg_interval
         end
+        println("Epoch $(epoch): train loss = " * string(loss_dict["train"]) * ", val loss = " * string(loss_dict["test"]))
     end
 end
 
